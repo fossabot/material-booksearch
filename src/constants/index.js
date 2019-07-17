5b0b8ac993638c6025e573a66d2cde9918e77499
Rishabh Jain <rishabh@jnexsoft.com>
2019-07-17 22:08:46 +0530

    added ES constants

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const esApp = {
+  app: 'new-book-search',
+  credentials: 'vrTi58e8o:04540063-5d81-4fb1-9969-a52d29892995',
+  url: 'https://scalr.api.appbase.io',
+  type: 'new-book-search',
+};
+
+
+export default { esApp };
