dbe4ea124360dd07e5b81475dc2124b51a961239
Rishabh Jain <rishabh@jnexsoft.com>
2019-07-17 22:15:58 +0530

    added packages reactive search, fragment, material

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue';
+import ReactiveSearch from '@appbaseio/reactivesearch-vue';
+import VueMaterial from 'vue-material';
+import { Plugin } from 'vue-fragment';
+
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+
+Vue.use(ReactiveSearch);
+Vue.use(VueMaterial);
+Vue.use(Plugin);
+
 
 new Vue({
   router,
