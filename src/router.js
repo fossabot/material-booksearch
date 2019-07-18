6246d32c64a5c814b9ff230467f1076a4478d4d1
Rishabh Jain <rishabh@jnexsoft.com>
2019-07-18 15:21:46 +0530

    fixed routes issue

--- src/router.js
+++ src/router.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Home from '@/views/Home.vue';
 
 Vue.use(Router);
 
 export const routes = [
-  [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+  },
 ];
 
 const router = new Router({ mode: 'history', routes });
