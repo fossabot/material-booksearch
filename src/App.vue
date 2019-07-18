afd89c1dc090b2542d00bbe83cc0d03aecd9ab4c
Rishabh Jain <rishabh@jnexsoft.com>
2019-07-18 19:43:04 +0530

    something is better then nothing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <v-toolbar app color="primary" :class="classes.appbar">
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span :class="classes.logoStyles">BS</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
@@ -22,7 +14,29 @@
 </template>
 
 <script>
+import { css } from 'emotion';
 import HelloWorld from './components/HelloWorld.vue';
+
+const logoStyles = css`
+  box-shadow: 2px 2px #1890ff, 4px 4px #096dd9;
+  padding: 10px;
+  font-weight: 600;
+  background: white;
+  color: #1890ff;
+  border-radius: 2px;
+  border: 1px solid #bae7ff;
+`;
+
+const appbarClass = theme => css`
+  z-index: ${theme.zIndex.drawer + 1};
+  flex-direction: row;
+  height: 64px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  box-shadow: none;
+  background: #174ff4;
+`;
 
 export default {
   name: 'App',
@@ -30,8 +44,14 @@ export default {
     HelloWorld,
   },
   data() {
+    const { theme } = this.$vuetify;
     return {
-      //
+      css,
+      classes: {
+        logoStyles,
+        appbar: appbarClass(theme),
+      },
+      message: 'saomthiung',
     };
   },
 };
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,13 +4,16 @@ import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
+    primary: '#174ff4',
+    secondary: '#eaeaea',
+    accent: '#90caf9',
     error: '#FF5252',
     info: '#2196F3',
     success: '#4CAF50',
-    warning: '#FFC107',
+    warning: '#FB8C00',
+    zIndex: {
+      drawer: 2,
+    },
   },
   iconfont: 'md',
 });
