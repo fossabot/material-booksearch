be74eee7ba9483292cbf9e05d56f87316d8beccc
Rishabh Jain <rishabh@jnexsoft.com>
2019-07-18 17:45:49 +0530

    added stylus

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import router from '@/router';
 import '@/plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '@/stylus/main.styl';
 
 Vue.config.productionTip = false;
 
