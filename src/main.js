facc2737083af37c466841c06e7821e186d54461
Rishabh Jain <rishabh@jnexsoft.com>
2019-07-22 14:16:26 +0530

    import fix for vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue';
 import ReactiveSearch from '@appbaseio/reactivesearch-vue';
-// import VueMaterial from 'vue-material';
 import { Plugin } from 'vue-fragment';
-
-// import 'vue-material/dist/vue-material.min.css';
-// import 'vue-material/dist/theme/default.css';
 
 import '@/plugins/vuetify';
 
@@ -13,13 +9,11 @@ import router from '@/router';
 
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import '@/stylus/main.styl';
 
 Vue.config.productionTip = false;
 
 
 Vue.use(ReactiveSearch);
-// Vue.use(VueMaterial);
 Vue.use(Plugin);
 
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import '@/stylus/main.styl';
 
 Vue.use(Vuetify, {
   theme: {
