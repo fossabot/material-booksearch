33b9dc1a53746fa05b59bede17f9b8c3e652caeb
Rishabh Jain <rishabh@jnexsoft.com>
2019-09-12 18:29:23 +0530

    style(lint): format doc for search component

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,17 +3,17 @@
     fluid
     wrap
   >
-      <Filters />
-      <v-layout :style="layoutStyle">
-        <SelectedFilters
-            clearAllLabel="Clear filters"
-            :showClearAll="true"
-        />
-      </v-layout>
-      <v-layout :style="layoutStyle">
-        <BookList
-          :gridProps="{ md4: true, sm6: true, xs12: true }"
-          :reactiveListProps="{
+    <Filters />
+    <v-layout :style="layoutStyle">
+      <SelectedFilters
+        clearAllLabel="Clear filters"
+        :showClearAll="true"
+      />
+    </v-layout>
+    <v-layout :style="layoutStyle">
+      <BookList
+        :gridProps="{ md4: true, sm6: true, xs12: true }"
+        :reactiveListProps="{
             showResultStats: false,
             react: {
               and: ['Ratings', 'Series', 'Authors', 'Search', 'Year'],
@@ -21,13 +21,13 @@
             size: 6,
             loader: null,
           }"
-          :cardProps="{
+        :cardProps="{
             style: {
               marginBottom: '24px',
             },
           }"
-        />
-      </v-layout>
+      />
+    </v-layout>
   </v-container>
 </template>
 
